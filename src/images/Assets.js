cfc082c0bc15310dfff13cd94e5a192a0e8e0bbb
Ian J Barker <ianjbark3r@gmail.com>
2020-07-15 11:41:15 -0500

    Added mobile version of photos to assets object

--- src/images/Assets.js
+++ src/images/Assets.js
@@ -1,7 +1,12 @@
 export const assets = {
+  glassesMobile: require('./glasses-photo-mobile.jpg'),
   glassesPhoto: require('./glasses-photo.jpg'),
+  jantiesMobile: require('./janties-photo-mobile.jpg'),
   jantiesPhoto: require('./janties-photo.jpg'),
+  hatsMobile: require('./hats-photo-mobile.jpg'),
   hatsPhoto: require('./hats-photo.jpg'),
+  storyMobile: require('./story-photo-mobile.jpg'),
   storyPhoto: require('./story-photo.jpg'),
+  aboutMobile: require('./about-photo-mobile.jpg'),
   aboutPhoto: require('./about-photo.jpg')
 };
